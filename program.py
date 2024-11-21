00bc8fc20247b243a44d16e2c309db2de66624b2
Christian Ramirez <christianemmanuelramirez0@gmail.com>
2024-11-21 22:57:17 +0800

    program with add record

--- program.py
+++ program.py
@@ -4,15 +4,24 @@ print("C - Clear All Records")
 print("D - Exit")
 choice = ""
 while choice.upper() != 'D':
- choice = input("ENTER SELECTION [A, B, C, or D]: ")
- if choice.upper() == 'A':
- print("Add Record")
- addRec()
- elif choice.upper() == 'B':
- print("View Records")
- viewRec()
- elif choice.upper() == 'C':
- print("Clear All Records")
- clearRec()
- elif choice.upper() == 'D':
- print("Thank you!")
+    choice = input("ENTER SELECTION [A, B, C, or D]: ")
+    if choice.upper() == 'A':
+        print("Add Record")
+        addRec()
+    elif choice.upper() == 'B':
+        print("View Records")
+        viewRec()
+    elif choice.upper() == 'C':
+        print("Clear All Records")
+        clearRec()
+    elif choice.upper() == 'D':
+        print("Thank you!")
+        
+def addRec(): 
+    file = open(filename, 'r') 
+    name = input("Enter Name: ") 
+    email = input("Enter Email: ") 
+    addr = input("Enter Address: ") 
+    with open(filename,'a') as file: 
+        file.write(name + ", " + email + ", " + addr + "\n") 
+        file.close()
